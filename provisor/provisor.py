6e884dd1380be4149daa9054eeb77329d99c2e80
Fox in the shell <KellerFuchs@hashbang.sh>
2016-04-05 01:02:21 +0000

    Fix indentation & formatting

--- provisor/provisor.py
+++ provisor/provisor.py
@@ -9,10 +9,12 @@ from exceptions import Exception
 from utils import make_salt
 from random import shuffle
 
+
 # An exception class for erroring-out
 #  on unknown hosts
 class UNKNOWN_HOST(Exception):
   pass
+
 
 class Provisor(object):
 
@@ -34,7 +36,7 @@ class Provisor(object):
     for (prop, default) in prop_defaults.iteritems():
         setattr(self, prop, kwargs.get(prop, default))
 
-    ldap.set_option(ldap.OPT_X_TLS_CACERTFILE,self.ca_certfile)
+    ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, self.ca_certfile)
     self.con = ldap.initialize(self.uri)
     self.con = ldap.ldapobject.ReconnectLDAPObject(
         self.uri,
@@ -61,14 +63,14 @@ class Provisor(object):
     stats = OrderedDict()
     server_list = self.servers()
     user_results = self.con.search_s(
-      self.user_base,
-      ldap.SCOPE_ONELEVEL,
-      '(objectClass=*)',
-      ("host",),
-      0
+        self.user_base,
+        ldap.SCOPE_ONELEVEL,
+        '(objectClass=*)',
+        ("host",),
+        0
     )
     for server in server_list:
-      stats[server['cn']] = stats.get(server['cn'],OrderedDict())
+      stats[server['cn']] = stats.get(server['cn'], OrderedDict())
       stats[server['cn']]['ip'] = server['ipHostNumber']
       stats[server['cn']]['location'] = server['l']
       stats[server['cn']]['currentUsers'] = stats[server['cn']].get('currentUsers', 0)
@@ -86,7 +88,7 @@ class Provisor(object):
     results = self.con.search_s(
         self.servers_base,
         ldap.SCOPE_ONELEVEL,
-        '(objectClass=*)', ("cn","maxUsers","l","ipHostNumber"),
+        '(objectClass=*)', ("cn", "maxUsers", "l", "ipHostNumber"),
         0
     )
     for r in results:
@@ -134,28 +136,25 @@ class Provisor(object):
       for attrs in r[1]:
         uids.append(int(r[1][attrs][0]))
     uids.sort()
-    for u in range(self.min_uid,self.max_uid,1):
+    for u in range(self.min_uid, self.max_uid, 1):
       if u in uids or u in self.excluded_uids:
         continue
       return u
-
 
   def add_group(self, groupname, gid):
     assert(gid >= self.min_uid)
     assert(gid <= self.max_uid)
 
     ml = {
-     'objectClass': [ 'top','posixGroup'],
-     'cn': [ groupname ],
-     'gidNumber': [ str(gid) ],
+        'objectClass': [ 'top', 'posixGroup' ],
+        'cn': [ groupname ],
+        'gidNumber': [ str(gid) ],
     }
     ml = ldap.modlist.addModlist(ml)
     self.con.add_s("cn={0},{1}".format(groupname, self.group_base), ml)
 
-
   def del_group(self, groupname):
     self.con.delete_s("cn={0},{1}".format(groupname, self.group_base))
-
 
   def is_group_member(self, group, user):
     try:
@@ -166,32 +165,29 @@ class Provisor(object):
     except ldap.NO_SUCH_OBJECT:
       return False
 
-
   def list_group_members(self, group):
     members = []
-    results = self.con.search_s("cn={0},{1}".format(group,self.group_base),
-                                      ldap.SCOPE_BASE, '(objectClass=*)', ("memberUid",), 0)
+    results = self.con.search_s("cn={0},{1}".format(group, self.group_base),
+                                ldap.SCOPE_BASE, '(objectClass=*)', ("memberUid",), 0)
     for r in results:
       for attrs in r[1]:
         for e in r[1][attrs]:
           members.append(e)
     return members
 
-
   def add_group_member(self, group, user):
     ml = { 'memberUid': [ user ] }
     ml = ldap.modlist.modifyModlist({}, ml, ignore_oldexistent=1)
     self.con.modify_s("cn={0},{1}".format(group, self.group_base), ml)
 
-
   def del_group_member(self, group, user):
-    old = self.con.search_s("cn={0},{1}".format(group, self.group_base), ldap.SCOPE_BASE, '(objectClass=*)', ("memberUid",), 0)
+    old = self.con.search_s("cn={0},{1}".format(group, self.group_base),
+                            ldap.SCOPE_BASE, '(objectClass=*)', ("memberUid",), 0)
     old = old[0][1]
     new = copy.deepcopy(old)
     new['memberUid'].remove(user)
     ml = ldap.modlist.modifyModlist(old, new)
     self.con.modify_s("cn={0},{1}".format(group, self.group_base), ml)
-
 
   """ Attempt to modify a users entry """
   def modify_user(self, username, pubkeys=None,
@@ -275,19 +271,18 @@ class Provisor(object):
     self.con.modify_s("uid={0},{1}".format(username, self.user_base), ml)
 
   """ Get User details """
-  def get_user(self,username):
+  def get_user(self, username):
     user = self.con.search_s("uid={0},{1}".format(username, self.user_base),
-            ldap.SCOPE_BASE, '(objectClass=*)', ("*",), 0)[0][1]
+                             ldap.SCOPE_BASE, '(objectClass=*)', ("*",), 0)[0][1]
     return user
-
 
   """ Adds a user, takes a number of optional defaults but the username and public key are required """
   def add_user(self, username, pubkey, hostname,
-                shell=None, homedir=None, password=None,
-                uid=None, lastchange=-1, nextchange=99999, warning=7, raw_passwd=None):
+               shell=None, homedir=None, password=None, uid=None,
+               lastchange=-1, nextchange=99999, warning=7, raw_passwd=None):
 
     if not homedir:
-      homedir="/home/{0}".format(username)
+      homedir = "/home/{0}".format(username)
 
     if hostname not in self.list_servers():
       raise UNKNOWN_HOST(hostname)
@@ -311,33 +306,32 @@ class Provisor(object):
       password = '{crypt}' + crypt.crypt(password, "$6${0}".format(make_salt()))
 
     ml = {
-      'objectClass': [ 'account', 'posixAccount', 'top' ,'shadowAccount', 'ldapPublicKey', 'inetLocalMailRecipient' ],
-      'uid' : [ username ],
-      'cn' : [ username],
-      'uidNumber' : [ str(uid) ],
-      'gidNumber' : [ str(gid) ],
-      'loginShell' : [ shell or self.default_shell ],
-      'homeDirectory' : [ homedir ],
-      'shadowLastChange' : [ str(lastchange) ],
-      'shadowMax' : [ str(nextchange) ],
-      'shadowWarning' : [ str(warning) ],
-      'shadowInactive' : [ str(99999) ],
-      'shadowExpire' : [ str(99999) ],
-      'userPassword' : [ str(password) ],
-      'sshPublicKey' : [ str(pubkey) ],
-      'host' : [ str(hostname) ],
-      'mailRoutingAddress' : [ '{0}@hashbang.sh'.format(username) ],
-      'mailHost' : [ str('smtp:'+hostname) ],
+        'objectClass': [ 'account', 'posixAccount', 'top', 'shadowAccount',
+                         'ldapPublicKey', 'inetLocalMailRecipient' ],
+        'uid': [ username ],
+        'cn': [ username],
+        'uidNumber': [ str(uid) ],
+        'gidNumber': [ str(gid) ],
+        'loginShell': [ shell or self.default_shell ],
+        'homeDirectory': [ homedir ],
+        'shadowLastChange': [ str(lastchange) ],
+        'shadowMax': [ str(nextchange) ],
+        'shadowWarning': [ str(warning) ],
+        'shadowInactive': [ str(99999) ],
+        'shadowExpire': [ str(99999) ],
+        'userPassword': [ str(password) ],
+        'sshPublicKey': [ str(pubkey) ],
+        'host': [ str(hostname) ],
+        'mailRoutingAddress': [ '{0}@hashbang.sh'.format(username) ],
+        'mailHost': [ str('smtp:' + hostname) ],
     }
 
     ml = ldap.modlist.addModlist(ml)
     self.con.add_s("uid={0},{1}".format(username, self.user_base), ml)
     self.add_group(username, gid)
 
-
   def del_user(self, username):
     self.con.delete_s("uid={0},{1}".format(username, self.user_base))
-
 
   def __del__(self):
     self.con.unbind_s()
